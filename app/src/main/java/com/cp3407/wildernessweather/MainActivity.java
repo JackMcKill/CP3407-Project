b48e1c978d61ebb574243bc7ee15d9531104c51f
Jack McGill <mr.mcgilljh@gmail.com>
2021-08-28 20:53:59 +1000

    Change request to a JsonObjectRequest.

--- app/src/main/java/com/cp3407/wildernessweather/MainActivity.java
+++ app/src/main/java/com/cp3407/wildernessweather/MainActivity.java
@@ -1,22 +1,17 @@
 package com.cp3407.wildernessweather;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,34 +25,19 @@ public class MainActivity extends AppCompatActivity {
 
         getWeatherData = findViewById(R.id.btn_getWeather);
         weatherData = findViewById(R.id.tv_weatherView);
-
-        getWeatherData.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-
-                //Instantiate the RequestQueue
-                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-                String url = "https://www.metaweather.com/api/location/search/?query=london";
-
-                // Request string response
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                // Add request to the RequestQueue
-                queue.add(stringRequest);
-            }
-        });
     }
 
+    public void getWeatherPressed(View view) {
+        // Instantiate the RequestQueue
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String url = "https://www.metaweather.com/api/location/1100661/"; // 1100661 is the code for Brisbane. Just hardcoded for now while prototyping.
 
+        // Request string response
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, response -> {
+            weatherData.setText(response.toString());
+            Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+        }, error -> Toast.makeText(MainActivity.this, "failed", Toast.LENGTH_SHORT).show());
+        // Add the request to the RequestQueue
+        queue.add(request);
+    }
 }
