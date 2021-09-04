3594d47bc7338d9017c1fb3f0c01e159f8566710
HarperT <harper.thurlow@my.jcu.edu.au>
2021-09-04 21:23:22 +1000

    App prints database as string to main screen

--- app/src/main/java/com/cp3407/wildernessweather/MainActivity.java
+++ app/src/main/java/com/cp3407/wildernessweather/MainActivity.java
@@ -2,30 +2,35 @@ package com.cp3407.wildernessweather;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.TestLooperManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.cp3407.wildernessweather.database.ConnectDatabase;
+import com.cp3407.wildernessweather.database.ConnectToDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    String x;
+
     Button apiPrototype;
-    TextView tv;
-    ConnectDatabase connectDatabase = new ConnectDatabase();
+    TextView databaseTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        databaseTextView = findViewById(R.id.connectionStatus);
 
-        connectDatabase.connectToDB();
-        System.out.println("x=" + x);
-        tv = findViewById(R.id.connectionStatus);
-        tv.setText(x);
+        // Begin ASync task to collect data from database.
+        ConnectToDatabase asyncTask = new ConnectToDatabase(new ConnectToDatabase.AsyncResponse() {
+            @Override
+            public void processFinish(String output) {
+                databaseTextView.setText( output);
+            }
+        });
+        asyncTask.execute();
+
+        //API Button
         apiPrototype = findViewById(R.id.btn_apiPrototype);
     }
 
