b26ae64a83160d6196b8b0232b768607f2ba78ac
HarperT <harper.thurlow@my.jcu.edu.au>
2021-09-04 12:12:32 +1000

    Dependencies not working with build.gradle

--- app/src/main/java/com/cp3407/wildernessweather/database/ConnectDatabase.java
+++ app/src/main/java/com/cp3407/wildernessweather/database/ConnectDatabase.java
@@ -6,7 +6,6 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cp3407.wildernessweather.R;
-import com.mysql.jdbc.Driver;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,7 +14,7 @@ import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 
 public class ConnectDatabase {
-    private static final String url = "jdbc:mysql://db-mysql-weatherapp.c5hi3iqblfad.ap-southeast-2.rds.amazonaws.com:3306/db-mysql-weatherapp";
+    private static final String url = "jdbc:mysql://db-mysql-weatherapp.c5hi3iqblfad.ap-southeast-2.rds.amazonaws.com:3306/db_mysql_weatherapp?useSSL=false";
     private static final String user = "admin";
     private static final String pass = "jackandharper";
 
@@ -24,11 +23,10 @@ public class ConnectDatabase {
     @SuppressLint("SetTextI18n")
     public String connectToDB(){
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection(url, user, pass);
             StringBuilder result = new StringBuilder("Database connected \n");
             Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery("select * from TestTable");
+            ResultSet rs = st.executeQuery("select * from Weatherapp");
             ResultSetMetaData rsmd = rs.getMetaData();
             while(rs.next()){
                 // This is will be changed to a loop to get all data from table (also setting differnt strings to display)
