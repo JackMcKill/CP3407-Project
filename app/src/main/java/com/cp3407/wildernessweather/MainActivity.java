f6d8fd1745644af49fa7542c55ca625d88ce370d
Jack McGill <mr.mcgilljh@gmail.com>
2021-09-05 14:13:46 +1000

    Add database code back to MainActivity.
    I accidentally merged incorrectly when resolving the merge conflict, however it is now fixed and back to normal.

--- app/src/main/java/com/cp3407/wildernessweather/MainActivity.java
+++ app/src/main/java/com/cp3407/wildernessweather/MainActivity.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.cp3407.wildernessweather.database.ConnectToDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     Button apiPrototype;
     Button dbPrototype;
+    TextView databaseTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
         apiPrototype = findViewById(R.id.btn_apiPrototype);
         dbPrototype = findViewById(R.id.dbPrototype);
+
+        databaseTextView = findViewById(R.id.connectionStatus);
+
+        // Begin ASync task to collect data from database.
+        ConnectToDatabase asyncTask = new ConnectToDatabase(new ConnectToDatabase.AsyncResponse() {
+            @Override
+            public void processFinish(String output) {
+                databaseTextView.setText( output);
+            }
+        });
+        asyncTask.execute();
     }
 
     public void apiPrototypePressed(View view) {
