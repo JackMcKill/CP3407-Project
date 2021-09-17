9fdcfb23b8b463e4bd8b9edc3953aa424e96a893
HarperT <harper.thurlow@my.jcu.edu.au>
2021-09-17 14:47:53 +1000

    Add #TODO's to code to complete to complete issue. Reformat current code to seperate functions for readability

--- app/src/main/java/com/cp3407/wildernessweather/database/ConnectToDatabase.java
+++ app/src/main/java/com/cp3407/wildernessweather/database/ConnectToDatabase.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ConnectToDatabase extends AsyncTask<Void, Void, String> {
@@ -18,30 +19,67 @@ public class ConnectToDatabase extends AsyncTask<Void, Void, String> {
     public AsyncResponse delegate = null; //Call back interface
 
     public ConnectToDatabase(AsyncResponse asyncResponse) {
-        delegate = asyncResponse;//Assigning call back interface through constructor
+        delegate = asyncResponse; //Assigning call back interface through constructor
     }
 
     @Override
     protected String doInBackground(Void... voids) {
         try{
-            Connection con = DriverManager.getConnection(url, user, pass);
-            StringBuilder result = new StringBuilder("Database connected \n");
-            Statement st = con.createStatement();
-            //Selects all from Test Table
-            ResultSet rs = st.executeQuery("select * from TestTable");
-            ResultSetMetaData rsmd = rs.getMetaData();
-            while(rs.next()){
-                // This is will be changed to a new function to get all data from table (also setting differnt strings to display)
-                result.append(rsmd.getColumnName(1)).append(": ").append(rs.getInt(1)).append("\n");
-                result.append(rsmd.getColumnName(2)).append(": ").append(rs.getString(2)).append("\n");
+            // #TODO Buttons blocked until connection made.
+            // Method made to connect to the database.
+            Connection con = connectToDatabase();
+            if (con != null){
+                // #TODO unblock buttons when connection made.
+
+                // #TODO if buttonPressed or onConditionMet then read database.
+
+                // Once connection is made read data from tables
+                return readDatabase(con);
+
+                // #TODO if buttonPressed or onConditionMet then write to database
+                // writeToDatabase();
             }
-            dataBaseOutput = (result.toString());
+            // #TODO add retry to connect to database button if connection failed
         }
         catch (Exception e){
             e.printStackTrace();
             dataBaseOutput = (e.toString());
         }
         return dataBaseOutput;
+    }
+
+    private String readDatabase(Connection con) throws SQLException {
+        // Once connection is made begin building output
+        StringBuilder result = new StringBuilder();
+        Statement st = con.createStatement();
+        //Selects all from Test Table
+        // #TODO needs to be changed to selecting all from different table
+        ResultSet rs = st.executeQuery("select * from TestTable");
+        ResultSetMetaData rsmd = rs.getMetaData();
+        while(rs.next()){
+            // #TODO Add for loop to count getColumnName(i) to set amount
+            result.append(rsmd.getColumnName(1)).append(": ").append(rs.getInt(1)).append("\n");
+            // #TODO use new WeatherReportModel to build from external database
+            result.append(rsmd.getColumnName(2)).append(": ").append(rs.getString(2)).append("\n");
+        }
+        dataBaseOutput = (result.toString());
+        return dataBaseOutput;
+    }
+
+    private Connection connectToDatabase(){
+        try{
+            return DriverManager.getConnection(url, user, pass);
+        } catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Could not connect to database");
+            dataBaseOutput = (e.toString());
+        }
+
+        return null;
+    }
+
+    private void writeToDatabase(){
+        // This function will act as a writing to database from local database data.
     }
 
     @Override
@@ -54,4 +92,5 @@ public class ConnectToDatabase extends AsyncTask<Void, Void, String> {
     public interface AsyncResponse {
         void processFinish(String output);
     }
+
 }
