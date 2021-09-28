1f3d95e1f2e18e00b3f2fc5a4e9059d22fb8c014
Jack McGill <mr.mcgilljh@gmail.com>
2021-09-28 11:08:28 +1000

    Revert "Merge pull request #29 from JackMcKill/settingsGUI"
    This reverts commit 6749fc0947ca4fa43850f32a67c8f1ee519f22ff, reversing
    changes made to 581e1dad0d1980035a1e7f6e826f39a8a13793c5.

--- app/src/main/java/com/cp3407/wildernessweather/MainActivity.java
+++ app/src/main/java/com/cp3407/wildernessweather/MainActivity.java
@@ -4,10 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.cp3407.wildernessweather.settings.SettingsActivity;
+import com.cp3407.wildernessweather.database.ConnectToDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,11 +33,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void btn_dbPrototypeClicked(View view) {
         Intent intent = new Intent(this, DbActivity.class);
-        startActivity(intent);
-    }
-
-    public void settingsPressed(View view) {
-        Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 }
