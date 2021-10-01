9dd5c09886226d3eb4a74d44a636c25b643b0195
Caleb Webster <caleb.webster@my.jcu.edu.au>
2021-10-01 15:27:55 +1000

    Rebase onto single_weather_report_view branch.
    Comment out findViewById() calls
    
    Add icon files.
    
    Add xml drawable for blue background image
    
    Complete layout design.
    
    Add barometer image.
    
    Convert sizes to percentages and set weightSums to 100.
    
    Set cityName for weatherReportModel to value provided in API call json object.
    
    Add method getStateImageResId() to resolve weather state abbreviations to image resources. This is used for the weather image. Uncomment populateFields() and findViewById() calls. Reformat setText() calls to round values and add units.
    
    Delete thermometer image asset.
    
    Add new images to show weather state.
    
    Add new image assets for values windDirectionCompass and predictability.
    
    Add TextView to show weather state and ImageView to show weather state image. Add new weather value fields for wind direction and predictability.
    
    Remove multiple .png files and replace with single .png files (don't know if this is a good idea or not).
    
    Refine layout weights and paddings, fix invisible icons.
    
    Fix HIGH temperature value not being filled correctly.
    
    Make blue box larger as it looked weird and was annoying me.
    
    Add layout_bottom_border.xml resource.
    
    Add method convertDateString() to convert date from YYYY-MM-DD format to DD-MM-YYYY format.
    
    Remove side padding from blue city name bar.
    
    Extract string resources, improve IDs, add comments.
    
    Add dummy functions and event listeners for buttons.

--- app/src/main/java/com/cp3407/wildernessweather/SingleWeatherReportActivity.java
+++ app/src/main/java/com/cp3407/wildernessweather/SingleWeatherReportActivity.java
@@ -1,10 +1,10 @@
 package com.cp3407.wildernessweather;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,26 +19,25 @@ public class SingleWeatherReportActivity extends AppCompatActivity {
     WeatherReportModel singleWeatherReport;
     WeatherReportViewModel viewModel;
 
-    TextView idView, stateView, stateAbbrView, windDirectionCompassView, createdView,
-            applicableDateView, minTempView, maxTempView, tempView, windspeedView, windDirectionView,
+    TextView cityNameView, stateView, applicableDateView, minTempView,
+            maxTempView, tempView, windSpeedView, windDirectionView,
             airPressureView, humidityView, visibilityView, predictabilityView;
 
+    ImageView stateImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_weather_report);
 
-        idView = findViewById(R.id.tv_id);
+        cityNameView = findViewById(R.id.tv_cityName);
         stateView = findViewById(R.id.tv_weatherStateName);
-        stateAbbrView = findViewById(R.id.tv_weatherStateAbbr);
-        windDirectionCompassView = findViewById(R.id.tv_windDirectionCompass);
-        createdView = findViewById(R.id.tv_created);
+        stateImage = findViewById(R.id.iv_stateImage);
         applicableDateView = findViewById(R.id.tv_applicableDate);
         minTempView = findViewById(R.id.tv_minTemp);
         maxTempView = findViewById(R.id.tv_maxTemp);
         tempView = findViewById(R.id.tv_weatherTemp);
-        windspeedView = findViewById(R.id.tv_windSpeed);
+        windSpeedView = findViewById(R.id.tv_windSpeed);
         windDirectionView = findViewById(R.id.tv_windDirection);
         airPressureView = findViewById(R.id.tv_airPressure);
         humidityView = findViewById(R.id.tv_humidity);
@@ -58,6 +57,20 @@ public class SingleWeatherReportActivity extends AppCompatActivity {
             // TODO code for when this activity is called without a Parcel object
         }
 
+        final ImageButton backButton = findViewById(R.id.btn_back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v) {
+                goBack();
+            }
+        });
+
+        final ImageButton downloadButton = findViewById(R.id.btn_download);
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v) {
+                downloadData();
+            }
+        });
+
     }
 
     // Adds the single weather report to the local database
@@ -68,20 +81,62 @@ public class SingleWeatherReportActivity extends AppCompatActivity {
     }
 
     public void populateFields() {
-        idView.setText(String.valueOf("ID: " + singleWeatherReport.getId()));
-        stateView.setText(String.valueOf("Weather State: " + singleWeatherReport.getWeatherStateName()));
-        stateAbbrView.setText(String.valueOf("Weather State Abbreviation: " + singleWeatherReport.getWeatherStateAbbr()));
-        windDirectionCompassView.setText(String.valueOf("Wind Direction Compass: " + singleWeatherReport.getWindDirectionCompass()));
-        createdView.setText(String.valueOf("Created: " + singleWeatherReport.getCreated()));
-        applicableDateView.setText(String.valueOf("Created: " + singleWeatherReport.getApplicableDate()));
-        minTempView.setText(String.valueOf("Min Temp: " + singleWeatherReport.getMinTemp()));
-        minTempView.setText(String.valueOf("Max Temp: " + singleWeatherReport.getMaxTemp()));
-        tempView.setText(String.valueOf("Weather Temp: " + singleWeatherReport.getTheTemp()));
-        windspeedView.setText(String.valueOf("Wind Speed: " + singleWeatherReport.getWindSpeed()));
-        windDirectionView.setText(String.valueOf("Wind Direction: " + singleWeatherReport.getWindDirection()));
-        airPressureView.setText(String.valueOf("Air Pressure: " + singleWeatherReport.getAirPressure()));
-        humidityView.setText(String.valueOf("Humidity: " + singleWeatherReport.getHumidity()));
-        visibilityView.setText(String.valueOf("Visibility: " + singleWeatherReport.getVisibility()));
-        predictabilityView.setText(String.valueOf("Predictability: " + singleWeatherReport.getPredictability()));
+
+        cityNameView.setText(String.valueOf(singleWeatherReport.getCityName()));
+        stateView.setText(String.valueOf(singleWeatherReport.getWeatherStateName()));
+        // Set weather state image field to correct resource.
+        stateImage.setImageResource(getStateImageResId(singleWeatherReport.getWeatherStateAbbr()));
+        // Convert date string before inserting.
+        applicableDateView.setText(String.valueOf(convertDateString(singleWeatherReport.getApplicableDate())));
+        minTempView.setText(String.valueOf(" " + Math.round(singleWeatherReport.getMinTemp()) + "°"));
+        maxTempView.setText(String.valueOf(" " + Math.round(singleWeatherReport.getMaxTemp()) + "°"));
+        tempView.setText(String.valueOf(" " + Math.round(singleWeatherReport.getTheTemp()) + "°"));
+        windSpeedView.setText(String.valueOf((Math.round(singleWeatherReport.getWindSpeed() * 100.0) / 100.0) + " Km/h"));
+        windDirectionView.setText(String.valueOf(singleWeatherReport.getWindDirectionCompass() + " (" + Math.round(singleWeatherReport.getWindDirection()) + "°)"));
+        airPressureView.setText(String.valueOf(singleWeatherReport.getAirPressure() + " in"));
+        humidityView.setText(String.valueOf(singleWeatherReport.getHumidity() + "%"));
+        visibilityView.setText(String.valueOf((Math.round(singleWeatherReport.getVisibility() * 100.0) / 100.0) + "mi"));
+        predictabilityView.setText(String.valueOf(singleWeatherReport.getPredictability() + "%"));
     }
+
+    /**
+     * Returns the resource id of the image that corresponds to a weather state abbreviation.
+     * For example:
+     * "lc" -> R.drawable.light_cloud
+     * "hr" -> R.drawable.heavy_rain
+     * @param abbreviation weather state abbreviation to get image for.
+     * @return resource id of the corresponding image.
+     */
+    public int getStateImageResId(String abbreviation) {
+        switch(abbreviation) {
+            case "c": return R.drawable.clear;
+            case "lc": return R.drawable.light_cloud;
+            default: return R.drawable.clear;
+        }
+    }
+
+    /**
+     * Converts date string in format YYYY-MM-DD to the format DD/MM/YYYY.
+     * @param dateString date string in old format.
+     * @return date string in new format.
+     */
+    public String convertDateString(String dateString) {
+        String[] parts = dateString.split("-");
+        return parts[2] + "/" + parts[1] + "/" + parts[0];
+    }
+
+    /**
+     * Returns to APIActivity.
+     */
+    public void goBack() {
+
+    }
+
+    /**
+     * Downloads weather data to the local database.
+     */
+    public void downloadData() {
+
+    }
+
 }
