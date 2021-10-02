22e8c7fa63bce2b8ad17a08512eae3bc17b88c8d
Caleb Webster <caleb.webster@my.jcu.edu.au>
2021-10-02 16:19:56 +1000

    Add method getStateImageResId() to resolve weather state abbreviations to image resources. This is used for the weather image. Uncomment populateFields() and findViewById() calls. Reformat setText() calls to round values and add units.

--- app/src/main/java/com/cp3407/wildernessweather/SingleWeatherReportActivity.java
+++ app/src/main/java/com/cp3407/wildernessweather/SingleWeatherReportActivity.java
@@ -1,10 +1,9 @@
 package com.cp3407.wildernessweather;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,32 +18,30 @@ public class SingleWeatherReportActivity extends AppCompatActivity {
     WeatherReportModel singleWeatherReport;
     WeatherReportViewModel viewModel;
 
-    TextView idView, stateView, stateAbbrView, windDirectionCompassView, createdView,
-            applicableDateView, minTempView, maxTempView, tempView, windspeedView, windDirectionView,
+    TextView cityNameView, stateView, applicableDateView, minTempView,
+            maxTempView, tempView, windSpeedView, windDirectionView,
             airPressureView, humidityView, visibilityView, predictabilityView;
 
+    ImageView stateImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_weather_report);
 
-//        idView = findViewById(R.id.tv_id);
-//        idView = findViewById(R.id.textView3);
-//        stateView = findViewById(R.id.tv_weatherStateName);
-//        stateAbbrView = findViewById(R.id.tv_weatherStateAbbr);
-//        windDirectionCompassView = findViewById(R.id.tv_windDirectionCompass);
-//        createdView = findViewById(R.id.tv_created);
-//        applicableDateView = findViewById(R.id.tv_applicableDate);
-//        minTempView = findViewById(R.id.tv_minTemp);
-//        maxTempView = findViewById(R.id.tv_maxTemp);
-//        tempView = findViewById(R.id.tv_weatherTemp);
-//        windspeedView = findViewById(R.id.tv_windSpeed);
-//        windDirectionView = findViewById(R.id.tv_windDirection);
-//        airPressureView = findViewById(R.id.tv_airPressure);
-//        humidityView = findViewById(R.id.tv_humidity);
-//        visibilityView = findViewById(R.id.tv_visibility);
-//        predictabilityView = findViewById(R.id.tv_predictability);
+        cityNameView = findViewById(R.id.tv_cityName);
+        stateView = findViewById(R.id.tv_weatherStateName);
+        stateImage = findViewById(R.id.iv_stateImage);
+        applicableDateView = findViewById(R.id.tv_applicableDate);
+        minTempView = findViewById(R.id.tv_minTemp);
+        maxTempView = findViewById(R.id.tv_maxTemp);
+        tempView = findViewById(R.id.tv_weatherTemp);
+        windSpeedView = findViewById(R.id.tv_windSpeed);
+        windDirectionView = findViewById(R.id.tv_windDirection);
+        airPressureView = findViewById(R.id.tv_airPressure);
+        humidityView = findViewById(R.id.tv_humidity);
+        visibilityView = findViewById(R.id.tv_visibility);
+        predictabilityView = findViewById(R.id.tv_predictability);
 
         viewModel = new ViewModelProvider(this).get(WeatherReportViewModel.class);
 
@@ -52,7 +49,7 @@ public class SingleWeatherReportActivity extends AppCompatActivity {
             Log.i("Parcelable", "Parcelable object received");
             // Get the report from the intent and unwrap it
             singleWeatherReport = (WeatherReportModel) Parcels.unwrap(getIntent().getParcelableExtra("report"));
-//            populateFields();
+            populateFields();
 
         } else {
             Log.i("Parcelable", "No parcelable object received");
@@ -69,20 +66,35 @@ public class SingleWeatherReportActivity extends AppCompatActivity {
     }
 
     public void populateFields() {
-//        idView.setText(String.valueOf("ID: " + singleWeatherReport.getId()));
-        stateView.setText(String.valueOf("Weather State: " + singleWeatherReport.getWeatherStateName()));
-        stateAbbrView.setText(String.valueOf("Weather State Abbreviation: " + singleWeatherReport.getWeatherStateAbbr()));
-        windDirectionCompassView.setText(String.valueOf("Wind Direction Compass: " + singleWeatherReport.getWindDirectionCompass()));
-        createdView.setText(String.valueOf("Created: " + singleWeatherReport.getCreated()));
-        applicableDateView.setText(String.valueOf("Created: " + singleWeatherReport.getApplicableDate()));
-        minTempView.setText(String.valueOf("Min Temp: " + singleWeatherReport.getMinTemp()));
-        minTempView.setText(String.valueOf("Max Temp: " + singleWeatherReport.getMaxTemp()));
-        tempView.setText(String.valueOf("Weather Temp: " + singleWeatherReport.getTheTemp()));
-        windspeedView.setText(String.valueOf("Wind Speed: " + singleWeatherReport.getWindSpeed()));
-        windDirectionView.setText(String.valueOf("Wind Direction: " + singleWeatherReport.getWindDirection()));
-        airPressureView.setText(String.valueOf("Air Pressure: " + singleWeatherReport.getAirPressure()));
-        humidityView.setText(String.valueOf("Humidity: " + singleWeatherReport.getHumidity()));
-        visibilityView.setText(String.valueOf("Visibility: " + singleWeatherReport.getVisibility()));
-        predictabilityView.setText(String.valueOf("Predictability: " + singleWeatherReport.getPredictability()));
+
+        cityNameView.setText(String.valueOf(singleWeatherReport.getCityName()));
+        stateView.setText(String.valueOf(singleWeatherReport.getWeatherStateName()));
+        stateImage.setImageResource(getStateImageResId(singleWeatherReport.getWeatherStateAbbr()));
+        applicableDateView.setText(String.valueOf(singleWeatherReport.getApplicableDate()));
+        minTempView.setText(String.valueOf(" " + Math.round(singleWeatherReport.getMinTemp()) + "°"));
+        minTempView.setText(String.valueOf(" " + Math.round(singleWeatherReport.getMaxTemp()) + "°"));
+        tempView.setText(String.valueOf(" " + Math.round(singleWeatherReport.getTheTemp()) + "°"));
+        windSpeedView.setText(String.valueOf((Math.round(singleWeatherReport.getWindSpeed() * 100.0) / 100.0) + " Km/h"));
+        windDirectionView.setText(String.valueOf(singleWeatherReport.getWindDirectionCompass() + " (" + Math.round(singleWeatherReport.getWindDirection()) + "°)"));
+        airPressureView.setText(String.valueOf(singleWeatherReport.getAirPressure() + " in"));
+        humidityView.setText(String.valueOf(singleWeatherReport.getHumidity() + "%"));
+        visibilityView.setText(String.valueOf((Math.round(singleWeatherReport.getVisibility() * 100.0) / 100.0) + "mi"));
+        predictabilityView.setText(String.valueOf(singleWeatherReport.getPredictability() + "%"));
+    }
+
+    /**
+     * Returns the resource id of the image that corresponds to a weather state abbreviation.
+     * For example:
+     * "lc" -> R.drawable.light_cloud
+     * "hr" -> R.drawable.heavy_rain
+     * @param abbreviation weather state abbreviation to get image for.
+     * @return resource id of the corresponding image.
+     */
+    public int getStateImageResId(String abbreviation) {
+        switch(abbreviation) {
+            case "c": return R.drawable.clear;
+            case "lc": return R.drawable.light_cloud;
+            default: return R.drawable.clear;
+        }
     }
 }
