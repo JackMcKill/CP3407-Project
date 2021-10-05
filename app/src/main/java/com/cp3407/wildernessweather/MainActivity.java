32d94d0087d73bb20d96597f4ab4ec48503aad10
CarolineYHao <39357148+CarolineYHao@users.noreply.github.com>
2021-10-05 13:30:42 +1000

    Rebase onto Develop

--- app/src/main/java/com/cp3407/wildernessweather/MainActivity.java
+++ app/src/main/java/com/cp3407/wildernessweather/MainActivity.java
@@ -1,44 +1,67 @@
 package com.cp3407.wildernessweather;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 
 
 import com.cp3407.wildernessweather.settings.SettingsActivity;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+    EditText searchBox;
+    ListView locationList;
+    TextView homeTitle, currentDate, locationName, editFavs;
+    ImageButton settingsButton;
 
-    Button apiPrototype;
-    Button dbPrototype;
+    String City[] = {"Brisbane", "Townsville"};
+    String minTemp[] = {"17", "25"};
+    String maxTemp[] = {"29", "30"};
+    int icons[] = {R.drawable.ic_c, R.drawable.ic_c};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        apiPrototype = findViewById(R.id.btn_apiPrototype);
-        dbPrototype = findViewById(R.id.dbPrototype);
+        homeTitle = findViewById(R.id.homeTitle);
+        currentDate = findViewById(R.id.currentDate);
+        locationName = findViewById(R.id.locationName);
+        editFavs = findViewById(R.id.editFav);
+        searchBox = findViewById(R.id.searchBox);
+        settingsButton = findViewById(R.id.settingsButton);
+
+        ListView locationList = findViewById(R.id.locationList);
+
+        ArrayAdapter
+
+
+
 
     }
 
-    public void apiPrototypePressed(View view) {
-        Intent intent = new Intent(this, APIactivity.class);
-        startActivity(intent);
-    }
 
-    public void btn_dbPrototypeClicked(View view) {
-        Intent intent = new Intent(this, DbActivity.class);
-        startActivity(intent);
-    }
 
-    public void settingsPressed(View view) {
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
-    }
+
+
 }
