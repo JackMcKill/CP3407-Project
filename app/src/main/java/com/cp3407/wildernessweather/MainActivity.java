8a3d8275e84a26fe3f4ade4d96fb4e1ef19d7cb2
Jack McGill <mr.mcgilljh@gmail.com>
2021-10-27 16:05:33 +1000

    Create API call to get weather information for favourite locations

--- app/src/main/java/com/cp3407/wildernessweather/MainActivity.java
+++ app/src/main/java/com/cp3407/wildernessweather/MainActivity.java
@@ -3,7 +3,7 @@ package com.cp3407.wildernessweather;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -19,6 +19,9 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences favourites;
+    private SharedPreferences settingsData;
+
+    private WeatherDataService weatherDataService;
 
     EditText searchBox;
     ListView locationList;
@@ -31,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         favourites = getSharedPreferences("favourites", Context.MODE_PRIVATE);
+        settingsData = getSharedPreferences("settings", Context.MODE_PRIVATE);
+
+        weatherDataService = new WeatherDataService(MainActivity.this);
 
         homeTitle = findViewById(R.id.homeTitle);
         currentDate = findViewById(R.id.currentDate);
@@ -44,27 +50,16 @@ public class MainActivity extends AppCompatActivity {
         initialiseList();
     }
 
+
     public void initialiseList() {
 
-        /*
-         * This method handles fetching the list of favourite locations from our SharedPreferences instance.
-         * It then generates an array of WeatherReportModels for the favourite locations and displays
-         * them in a simple list adapter.
-         *
-         * Because we have not yet implemented a way for users to add locations to their favourites list,
-         * we can generate some dummy information in the meantime (dummySetSharedPreferences()).
-         *
-         * @Caroline once you have finished implementing the custom ArrayAdapter, we can replace the
-         * current ArrayAdapter in this method with the new one, and pass in the array of WeatherReportModels (favLocationReports)
-         * instead of the array of Strings (cityNames)
-         */
-
-        dummySetSharedPreferences(); // sets some favourite locations into SharedPreferences (delete this once locations can be added to sharedPrefs by the user
-        String[] cityNames = getFavCityNames(); // gets favourite locations from SharedPreferences and puts them into an array
-        WeatherReportModel[] favLocationReports = getWeatherReports(cityNames); // generates an array of weather reports using the array of city names
-
-        ArrayAdapter<String> simpleArrayAdapterDEMO = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, cityNames);
-        locationList.setAdapter(simpleArrayAdapterDEMO);
+        dummySetSharedPreferences();
+        String[] cityNames = getFavCityNames();
+        Log.i("main", "about to get weather reports");
+        weatherDataService.getFavourites(cityNames, weatherReportModels -> {
+            WeatherReportModelListAdapter adapter = new WeatherReportModelListAdapter(MainActivity.this, R.layout.weather_report_list_item, weatherReportModels);
+            locationList.setAdapter(adapter);
+        });
     }
 
     public void dummySetSharedPreferences() {
@@ -88,23 +83,5 @@ public class MainActivity extends AppCompatActivity {
 
     public String[] convertSetToArray(Set<String> setOfString) {
         return setOfString.toArray(new String[0]);
-    }
-
-    // Generates a list of WeatherReports
-    public WeatherReportModel[] getWeatherReports(String[] names) {
-        WeatherReportModel[] reports = new WeatherReportModel[names.length];
-        int i = 0;
-        for (String location : names) {
-            // initialise a new WeatherReportModel
-            WeatherReportModel report = new WeatherReportModel();
-
-            // make an API call to get the data
-            report.setCityName(location); // replace this line with API call
-
-            // add to the array
-            reports[i] = report;
-            i++;
-        }
-        return reports;
     }
 }
